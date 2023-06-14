c4ffe8a3e9425f0e8ca82c9bfd5668cdd0a24ea1
jeryelblanco <jeryelblanco@gmail.com>
2023-06-14 13:57:04 -0400

    Updated Routes

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 from models import db, Blogs
 
@@ -16,13 +16,55 @@ def root_page():
         'Welcome to my database', 200
     )
 
-@app.route('/blogs')
+@app.route('/blogs', methods = ["GET", "POST", "DELETE"])
 def my_blogs():
-    blog_list = [this.to_dict() for this in Blogs.query.all()]
-    return make_response(
-    blog_list,
-    200
-    )
+    if request.method == 'GET':
+        blog_list = [this.to_dict() for this in Blogs.query.all()]
+        return make_response(
+        blog_list,
+        200
+        )
+    if request.method == 'POST':
+        form_data = request.get_json()
+        title_data = form_data.get('title')
+        image_data = form_data.get('image')
+        text_data = form_data.get('text')
+        new_blog = Blogs(
+        title = title_data,
+        image = image_data,
+        text = text_data
+        )
+        db.session.add(new_blog)
+        db.session.commit()
+        return make_response(
+        new_blog.to_dict()
+        )
+ 
+@app.route('/blogs/<int:id>', methods = ["GET", "DELETE"])
+def blog_by_id(id):
+    if request.method == 'GET':
+        id_data = id
+        blog_specific = Blogs.query.filter_by(id = id_data).first()
+        return make_response(
+        blog_specific.to_dict(),
+        200
+        )
+    
+    if request.method == 'DELETE':
+        id_data = id
+        blog_query = Blogs.query.filter_by(id = id_data).first()
+        db.session.delete(blog_query)
+        db.session.commit()
+        return make_response(
+        jsonify({f"Blog with title of {blog_query.title} and id of: {id_data}": "has been deleted"})
+        )
+    pass
+        
+
+    
+    
+    
+    
     
 if __name__ == "__main__":
     app.run(port = 5555, debug=True)
